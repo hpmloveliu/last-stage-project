709bb00df098e2ba5499463e4c1b8dc6dbd4c4e6
hpm <876288830@qq.com>
2018-12-17 22:24:36 +0800

    "this is project-init"

---
+++ src/main/java/com/baizhi/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.baizhi.controller;
+
+import com.baizhi.entity.User;
+import com.baizhi.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    //DI
+    @Autowired
+    private UserMapper userMapper;
+
+    @RequestMapping("queryUser")
+    public void queryUser(){
+        System.out.println("123213");
+        List<User> users = userMapper.selectAll();
+        for (User user : users) {
+            System.out.println("user::"+user);
+        }
+    }
+}
